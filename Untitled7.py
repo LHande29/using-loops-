7b5d787112735e6509cc03d52857cf6a8f581d28
LHande29 <91967917+LHande29@users.noreply.github.com>
2021-10-14 18:57:24 +0530

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+nums = [12,-7,5,64,-14]
+nums = list(filter( lambda x : x>0,nums))
+print("Positive numbers in the list",nums)
+
+
+# In[9]:
+
+
+nums = [12,14,-95,3]
+nums = list(filter( lambda x : x>0,nums)) 
+print ("Positive numbers in th elist",nums)
+
+
+# In[ ]:
+
+
+
+
